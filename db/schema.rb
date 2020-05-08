18735e1a3acb3a413a86daa636b296ebfd72a55b
Pol-S <plsbnjy@gmail.com>
2020-05-07 20:58:36 -0500

    Added the character_spell model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_014724) do
+ActiveRecord::Schema.define(version: 2020_05_08_015602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_05_08_014724) do
   create_table "character_classes", force: :cascade do |t|
     t.string "character_class"
     t.integer "character_class_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "character_spells", force: :cascade do |t|
+    t.integer "character_id"
+    t.integer "spell_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
