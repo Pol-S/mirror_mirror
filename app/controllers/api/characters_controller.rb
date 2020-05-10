44d15a0d3570d3e16132b815a00dd831ba07afc8
Pol-S <plsbnjy@gmail.com>
2020-05-10 14:50:25 -0500

    added index action for characters, no log in yet

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,2 +1,18 @@
 class Api::CharactersController < ApplicationController
+  def index
+    @all_characters = Character.all
+    render "index.json.jb"
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
     post "/users" => "users#create"
 
     post "/sessions" => "sessions#create"
+
+    get "/characters" => "characters#index"
+    get "/characters/:id" => "characters#show"
+    post "/characters" => "characters#create"
+    patch "/characters" => "characters#update"
+    delete "/characters" => "characters#destroy"
   end
 end
